c35091bfbcb2ed9f49556f4500e600b4be1fb175
NHKhang132 <19521665@gm.uit.edu.vn>
2022-11-22 21:58:06 +0700

    hard code Building Name + Check Block name

--- controllers/insert.controller.js
+++ controllers/insert.controller.js
@@ -3,16 +3,22 @@ const FaceService = require('../services/face.service')
 const BlockService = require("../services/block.service");
 
 const BuildingModel = require("../models/building.model");
-
+const BlockModel = require("../models/block.model");
 
 async function handleBlock(req, res, next) {
     let geoJson = req.body.geoJson;
     let renderer = req.body.renderer;
 
     //Check Building đã tồn tại hay chưa
-    const building = await BuildingModel.findOne({building_name: req.body.building_name})
-    if (!building) {
-        return res.status(200).json({ message: 'BUILDING KHÔNG TỒN TẠI!!! VUI LÒNG TẠO BUILDING!!!' });
+    // const building = await BuildingModel.findOne({building_name: req.body.building_name})
+    // if (!building) {
+    //     return res.status(200).json({ message: 'BUILDING KHÔNG TỒN TẠI!!! VUI LÒNG TẠO BUILDING!!!' });
+    // }
+
+    //Check Block đã tồn tại hay chưa
+    const existBlock = await BlockModel.findOne({ blockName: geoJson?.features[0]?.properties?.Name })
+    if (existBlock) {
+        return res.status(200).json({ message: 'BLOCK ĐÃ TỒN TẠI!!!' });
     }
 
     let listNodeId = []
@@ -43,9 +49,9 @@ async function handleBlock(req, res, next) {
     }
     const savedBlock = await BlockService.saveBlock(block)
 
-    const updatedBuilding = await BuildingModel.findByIdAndUpdate(
+    const updatedBuilding = await BuildingModel.findOneAndUpdate(
         { 
-            _id: building._id 
+            building_name: 'Dragon Wharf' 
         },
         {
             $push: { blocks: savedBlock._id }
